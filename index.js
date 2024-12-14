15cd3931c4675ee54b343b36497ff8bf2e99f209
Fernando Ortega <128515496+FernandoOrtegaS@users.noreply.github.com>
2024-12-14 03:47:51 +0000

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { engine } from 'express-handlebars';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 
-const sql = neon('postgresql://neondb_owner:rpxY60mWIvHG@ep-nameless-scene-a5cyx3xc.us-east-2.aws.neon.tech/neondb?sslmode=require');
+const sql = neon('postgresql://neondb_owner:HL5t9kjbCpEa@ep-winter-scene-a5bjck6t.us-east-2.aws.neon.tech/neondb?sslmode=require');
 
 const app = express();
 
@@ -25,6 +25,38 @@ app.get('/', async (req, res) => {
 app.get('/asistencia', (req, res) => {
     res.render('asistencia');
 })
+
+// Este endpoint parece estar sobrando y podría estar causando problemas
+// app.get('/notas', (req, res) => {
+//     res.render('notas');
+// });
+
+app.get('/notas', async (req, res) => {
+    try {
+        
+        const result = await sql`
+            SELECT * 
+            FROM notasalumnos 
+        `;
+  
+        if (result.length > 0) {
+            res.render('notas', { alumnos: result });
+        } else {
+            res.render('notas', { alumnos: [] });
+        }
+    } catch (err) {
+        console.error('Error al obtener los préstamos no devueltos', err);
+        res.status(500).send('Error al obtener los préstamos no devueltos');
+    }
+  });
+
+
+
+
+
+
+
+
 
 
 const port = process.env.PORT || 3003;
